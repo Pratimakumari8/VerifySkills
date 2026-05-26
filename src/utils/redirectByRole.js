6b7f46875c6d3ff89a7057e60cd7a3b9b5095074
Pratima Kumari <kumaripratima337@gmail.com>
2026-05-26 17:52:28 +0530

    Initial empty setup for solana contracts

--- src/utils/redirectByRole.js
+++
@@ -1,7 +0,0 @@
-export const getDashboardPath = (role) => {
-  if (role === "Institute") return "/institute-dashboard";
-  if (role === "Certificate holder") return "/dashboard";
-  if (role === "Employer") return "/employer-dashboard";
-
-  return "/";
-};
