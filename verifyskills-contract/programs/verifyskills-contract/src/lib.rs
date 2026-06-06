503ad7f047d4f8900afd41c0ed67eff22d248551
Pratima Kumari <kumaripratima337@gmail.com>
2026-06-06 12:00:26 +0530

    ok

---
+++ verifyskills-contract/programs/verifyskills-contract/src/lib.rs
@@ -0,0 +1,21 @@
+pub mod constants;
+pub mod error;
+pub mod instructions;
+pub mod state;
+
+use anchor_lang::prelude::*;
+
+pub use constants::*;
+pub use instructions::*;
+pub use state::*;
+
+declare_id!("Db5UP4RJdvubepNdsQN6Cgk8sGocmzZiqgZTrVHhDwcR");
+
+#[program]
+pub mod verifyskills_contract {
+    use super::*;
+
+    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+        initialize::handler(ctx)
+    }
+}
